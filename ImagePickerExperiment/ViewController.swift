b74ca2354c313dae32c7a9d094e6477226aa9c5d
Jim Barry <jbarry@pelicom.net>
2019-07-20 13:37:29 -0500

    initial commit

---
+++ ImagePickerExperiment/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ImagePickerExperiment
+//
+//  Created by James Barry on 7/3/19.
+//  Copyright © 2019 Jim Barry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        present(imagePicker, animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
